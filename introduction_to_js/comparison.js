0db7eebd6067d4c8f8f9966c4a86b69579a1fdff
athul-s-m <athulsmrahul@gmail.com>
2025-09-19 14:30:36 +0530

    comparison of strings and numbers

---
+++ introduction_to_js/comparison.js
@@ -0,0 +1,12 @@
+const numberOne = 10;
+const numberTwo = 20;
+
+const fname = "athul";
+const sname = "athul";
+
+const isNumbersAreSame = numberOne === numberTwo;
+const isNamesAreSame = fname === sname;
+
+
+console.log(isNumbersAreSame);
+console.log(isNamesAreSame);
