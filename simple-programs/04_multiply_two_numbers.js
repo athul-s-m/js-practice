dc4202fd4486441e56479bcac54b116922688e64
Athul S M <athulsmrahul@gmail.com>
2025-09-29 09:12:35 +0530

    Create 04_multiply_two_numbers.js

---
+++ simple-programs/04_multiply_two_numbers.js
@@ -0,0 +1,14 @@
+function multiplication(multiplier, multiplicand) {
+  return multiplier * multiplicand;
+}
+
+function main() {
+  const multiplier = 10;
+  const multiplicand = 20;
+
+  const product = multiplication(multiplier, multiplicand);
+  
+  console.log("Product: " + product);
+}
+
+main();
