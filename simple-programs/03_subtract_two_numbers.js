df4e4f23f25b07001da05974f998e81f0793b029
Athul S M <athulsmrahul@gmail.com>
2025-09-29 09:11:59 +0530

    Create 03_subtract_two_numbers.js

---
+++ simple-programs/03_subtract_two_numbers.js
@@ -0,0 +1,14 @@
+function subtract(minuend, subtrahend) {
+  return minuend - subtrahend;
+}
+
+function main() {
+  const minuend = 100;
+  const subtrahend = 50;
+
+  const difference = minuend - subtrahend;
+
+  console.log("Difference: " + difference);
+}
+
+main();
