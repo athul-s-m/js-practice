bbc9010464788645e379e36994ac6277b4c6bed3
Athul S M <athulsmrahul@gmail.com>
2025-09-29 09:13:12 +0530

    Create 05_divide_two_numbers.js

---
+++ simple-programs/05_divide_two_numbers.js
@@ -0,0 +1,14 @@
+function division(dividend, divisor) {
+  return dividend / divisor;
+}
+
+function main() {
+  const dividend = 10;
+  const divisor = 20;
+
+  const quotient = division(dividend, divisor);
+  
+  console.log("Quotient: " + quotient);
+}
+
+main();
