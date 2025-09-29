48cf222e5d71e5c364a417df42283a018662a89e
Athul S M <athulsmrahul@gmail.com>
2025-09-29 09:11:26 +0530

    Create 02_add_two_numbers.js

---
+++ simple-programs/02_add_two_numbers.js
@@ -0,0 +1,14 @@
+function sum(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function main() {
+  const firstNumber = 10;
+  const secondNumbe = 20;
+
+  const result = sum(firstNumber, secondNumbe);
+  
+  console.log("Sum: " + result);
+}
+
+main();
